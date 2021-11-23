7406b9018052abb879a0d5c441ef1e376b7779ce
nasta <nasta@LAPTOP-9GRLQUF7>
2021-11-24 00:26:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Запросить у пользователя целочисленное значение N. Сформировать двумерный массив размера NxN 
+            int[] array = new int[5];
+            
+            Console.WriteLine("Количество элементов массива - 5");
+            Console.WriteLine();
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Введите элемент массива под индексом {i}: ");
+                array[i] = int.Parse(Console.ReadLine());
+                
+            }
+            //Если число четное, будет 1 0 1 0 1; нечетное - 0 1 0 1 0
+            Console.WriteLine();
+            Console.WriteLine("Вывод массива: ");
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] % 2 == 0)
+                {
+                    Console.WriteLine("1 0 1 0 1");
+                }
+                else
+                {
+                    Console.WriteLine("0 1 0 1 0");
+                }
+            Console.WriteLine();
+            Console.WriteLine("Для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Сформировать одномерный массив из 7 элементов.
+            Заполнить массив числами, вводимыми с клавиатуры, определить среднее арифметическое элементов.*/
+            int[] array = new int[7];
+            int S = 0;
+            Console.WriteLine("Количество элементов массива - 7");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Введите элемент массива под индексом {i}: ");
+                array[i] = int.Parse(Console.ReadLine());
+                S += array[i];
+            }
+
+            Console.WriteLine("Вывод массива: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.Write("Среднее арифметическое: ");
+            Console.WriteLine("{0,8:f2}", S / 7);
+            Console.WriteLine("Для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
